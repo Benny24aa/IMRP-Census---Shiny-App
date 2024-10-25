ac54e2df674ab3f2d009994d9327087defb0d010
Benny24aa <harleyb101010@gmail.com>
2024-10-25 19:06:06 +0100

    tier 1/0 added into dataset for analysis

--- data prep/War Tiers Setup.R
+++ data prep/War Tiers Setup.R
@@ -5,15 +5,21 @@ Sniper_List_Killer <- IMRP_Full_Census_File %>%
   group_by(killerName, War_ID) %>% 
   summarise(count=n(), .groups = 'drop') %>% 
   select(-count) %>% 
-  filter(killerName != is.na(killerName))
+  filter(killerName != is.na(killerName)) %>% 
+  mutate(Tier_Killer= "Tier 1/0")
  
 
 Sniper_List_Death <- IMRP_Full_Census_File %>% 
   select(killerName, killerId, War_ID, reason) %>% 
   filter(reason == "34") %>% 
   select(-reason) %>% 
-  rename(KilledName = killerName) %>% 
-group_by(KilledName, War_ID) %>% 
+  rename(killedName = killerName) %>% 
+group_by(killedName, War_ID) %>% 
   summarise(count=n(), .groups = 'drop') %>% 
   select(-count)%>% 
-  filter(KilledName != is.na(KilledName))
+  filter(killedName != is.na(killedName)) %>% 
+  mutate(Tier_Death = "Tier 1/0")
+
+IMRP_Full_Census_File_Cleaned <- full_join(Sniper_List_Killer, IMRP_Full_Census_File, by = c("killerName", "War_ID"))
+
+IMRP_Full_Census_File_Cleaned <- full_join(Sniper_List_Death, IMRP_Full_Census_File_Cleaned, by=c("killedName", "War_ID"))
