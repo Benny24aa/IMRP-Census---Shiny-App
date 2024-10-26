86290129fec8ffa80d6a5cc56958a33a6fa8ae83
Benny24aa <harleyb101010@gmail.com>
2024-10-26 10:54:19 +0100

    front page code added

--- rapp/ui.R
+++ rapp/ui.R
@@ -1,2 +1,23 @@
 
 source("C:/Users/benny/Documents/My Resps/IMRP-Census---Shiny-App/Information Page Scripts/information page ui.R")
+
+navbarPage(title = div(tags$a(img(src="", width=120, alt = ""),
+                              href= "",
+                              target = "_blank"),
+                       style = "position: relative; top: -10px;"),
+           windowTitle = "IMRP Census 2024", #title for browser tab
+           header = tags$head(includeCSS("C:/Users/benny/Documents/My Resps/IMRP-Census---Shiny-App/www/styles.css"), # CSS styles
+                              HTML("<html lang='en'>")),
+           
+           ##### Tab Panels
+           information, ### Information Tab being called on by UI
+           navbarMenu("Leaderboards"),# Leaderboards
+           navbarMenu("Location Analysis") # Location Analysis
+           
+           
+           
+           
+           
+           
+           
+)#End of navbarpage
