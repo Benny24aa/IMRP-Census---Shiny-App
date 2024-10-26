1d082cd10330a09e39ead02ae76ffc25040e18a3
Benny24aa <harleyb101010@gmail.com>
2024-10-26 10:49:03 +0100

    sourcing in files

---
+++ rapp/server.R
@@ -0,0 +1,5 @@
+function(input, output) {
+  source(file.path("C:/Users/benny/Documents/My Resps/IMRP-Census---Shiny-App/Information Page Scripts/information page server.R"), local = TRUE)$value
+  
+  
+}
