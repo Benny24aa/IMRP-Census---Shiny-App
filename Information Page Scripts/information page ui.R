fb2b68940738b791d13042a740ecd3ff2ecf4d88
Benny24aa <harleyb101010@gmail.com>
2024-10-26 10:48:54 +0100

    added information pages server and ui

---
+++ Information Page Scripts/information page ui.R
@@ -0,0 +1,21 @@
+information <- tabPanel(title = "Information", 
+                        icon = icon("info-circle"),
+                        fluidRow(
+                          column(6,
+                                 h2("Welcome to the IMRP Census Dashboard", style = "color:  #336699 ; font-weight: 600"))),
+                        
+                        fluidRow(
+                          column(6, actionButton("new_next", tags$b("New content and future updates"),
+                                                 icon = icon('calendar-alt')))),
+                        
+                        fluidRow(
+                          column(12,
+                                 h4(tags$b("What is the NHS Open Data Dashboard?" , style = "color:  #336699 ; font-weight: 600")),
+                                 p(" ."), 
+                                 h4(tags$b("Type of Data Explored?", style = "color:  #336699 ; font-weight: 600" )),
+                                 p("."),
+                                 h4(tags$b(" Disclosure and Data Security Statement", style = "color:  #336699 ; font-weight: 600")),
+                                 p(".")),
+                        )#End of Fluid Row
+                        
+) #End of TabPanel
