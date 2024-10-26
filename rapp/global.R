f80a3ec5f431f9e62a047035e0218285504e8e3c
Benny24aa <harleyb101010@gmail.com>
2024-10-26 10:39:59 +0100

    sourcing in data preps to global script so the app can call on it

--- rapp/global.R
+++ rapp/global.R
@@ -1,2 +1,5 @@
 source(file.path('C:/Users/benny/Documents/My Resps/IMRP-Census---Shiny-App/data prep/Data Prep.R'))
 source(file.path('C:/Users/benny/Documents/My Resps/IMRP-Census---Shiny-App/data prep/War Tiers Setup.R'))
+source(file.path("C:/Users/benny/Documents/My Resps/IMRP-Census---Shiny-App/data prep/player quintle lookup.R"))
+source(file.path("C:/Users/benny/Documents/My Resps/IMRP-Census---Shiny-App/data prep/Region Analysis.R"))
+source(file.path("C:/Users/benny/Documents/My Resps/IMRP-Census---Shiny-App/data prep/Leaderboards Rmarkdown Prep.R"))
