5b272be33e10c531894b0882161a1ecf1e17e741
Benny24aa <harleyb101010@gmail.com>
2024-10-31 18:49:15 +0000

    UI kd added

--- rapp/data prep/Active War/Active War KD UI.R
+++ rapp/data prep/Active War/Active War KD UI.R
@@ -1,13 +1,9 @@
 Active_War_KDs_UI <- tabPanel(title = "Kill/Death Ratios", icon = icon('table'),
-                              h2("Select the type of kill/death ratios you wish to see", style = "color: #336699 ; font-weight: 600"),
-                              p("This section allows you to view kd data in table format. You can use the filters to select the data you're interested in and download it into a csv format using the download button."),
-                              column(6, selectInput("KD_Type_Input", "Select the data you want to explore.",
+                              h2("Select the KD's you wish to download", style = "color: #336699 ; font-weight: 600"),
+                              p("This section allows you to view kill/death ratio data in table format. You can use the filters to select the data you're interested in and download it into a csv format using the download button."),
+                              column(6, selectInput("ref_select", "Select the data you want to explore.",
                                                     choices = Table_KD_List)),
                               mainPanel(width = 12,
-                                        DT::dataTableOutput("KD_Table_Filtered")),
-                              column(6, downloadButton('download_table_csv_kd', 'Download data'))
-                              
-                              
-                              
-                              ) #end of tab
-
+                                        DT::dataTableOutput("table_filtered")),
+                              column(6, downloadButton('download_table_csv', 'Download data')),
+) 
