6b381be41f4dbf49b235b90aa73a22f9157dd22c
Benny24aa <harleyb101010@gmail.com>
2024-11-01 00:01:25 +0000

    total percentage graph added to ui

--- rapp/data prep/Active War/Active War Deaths Analysis UI.R
+++ rapp/data prep/Active War/Active War Deaths Analysis UI.R
@@ -4,7 +4,10 @@ Active_War_Deaths <- tabPanel(title = "Death Analysis", icon = icon('hospital'),
                               fluidRow(
                                 column(3, selectInput(inputId = "death_analysis_killed_name_input", label = "Select type of data you wish to see", choices = unique(Active_War_Death_Graph_Prep$killedName)))),
                                 fluidRow(
-                                  column(3, plotlyOutput("total_deaths_by_player_per_day_graph", width = "300%", height = "600px")))
-                              
+                                  column(3, plotlyOutput("total_deaths_by_player_per_day_graph", width = "300%", height = "600px"))),
+                              h1("Total Percentage of Deaths Across Entire by Selected Played Daily", style = "color: #336699 ; font-weight: 600"),
+                              p("Total Percentage of Deaths by Selected Played Daily"),
+                              fluidRow(
+                                column(3, plotlyOutput("total_percentage_by_player_per_day_graph", width = "300%", height = "600px")))
                               
                               )
