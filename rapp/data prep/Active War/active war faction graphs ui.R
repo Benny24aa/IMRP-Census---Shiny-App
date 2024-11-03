fd335c26279a32cb58a2fddb888c1f6683de7fd8
Benny24aa <harleyb101010@gmail.com>
2024-11-03 03:40:25 +0000

    new graph added to ui

--- rapp/data prep/Active War/active war faction graphs ui.R
+++ rapp/data prep/Active War/active war faction graphs ui.R
@@ -1,5 +1,16 @@
 Factions_Active_War_UI <- tabPanel(title = "War Overview", icon = icon('hospital'),
                               h1("Number of Deaths Daily by Faction", style = "color: #336699 ; font-weight: 600"),
-                              p("Number of Deaths Daily by Faction"),
+                              p("Number of Deaths Daily by Faction since the start of the war"),
                               fluidRow(
-                                column(3, plotlyOutput("active_war_graph_overview", width = "300%", height = "600px"))))
+                                column(3, plotlyOutput("active_war_graph_overview", width = "300%", height = "600px"))),
+                              h1("Total Number of Deaths by Faction", style = "color: #336699 ; font-weight: 600"),
+                              p("Total number of deaths since the start of the war by faction"),
+                              fluidRow(
+                                column(3, plotlyOutput("active_war_graph_overview_cum", width = "300%", height = "600px")))
+                              
+                           
+                              
+                              
+                              
+                              
+                              )
