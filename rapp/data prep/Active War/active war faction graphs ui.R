89035a163b5ba5cc7ef319d4226b921bcc60a825
Benny24aa <harleyb101010@gmail.com>
2024-11-03 04:14:11 +0000

    added playtime graph to ui

--- rapp/data prep/Active War/active war faction graphs ui.R
+++ rapp/data prep/Active War/active war faction graphs ui.R
@@ -6,7 +6,11 @@ Factions_Active_War_UI <- tabPanel(title = "War Overview", icon = icon('hospital
                               h1("Total Number of Deaths by Faction", style = "color: #336699 ; font-weight: 600"),
                               p("Total number of deaths since the start of the war by faction"),
                               fluidRow(
-                                column(3, plotlyOutput("active_war_graph_overview_cum", width = "300%", height = "600px")))
+                                column(3, plotlyOutput("active_war_graph_overview_cum", width = "300%", height = "600px"))),
+                              h1("Total Number of Hours Daily by Faction", style = "color: #336699 ; font-weight: 600"),
+                              p("Total number of hours played since the start of the war."),
+                              fluidRow(
+                                column(3, plotlyOutput("playtime_active_war_graph", width = "300%", height = "600px")))
                               
                            
                               
